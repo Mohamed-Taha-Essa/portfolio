ae8eee1fba8a5931759ca50ecf851eddd2cfa96a
Mohamed Abdallah Essa <www.taha.166@gmail.com>
2026-01-12 23:38:23 +0200

    send dynamic data in index method

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .models import Home, About, SkillSection, Skill, WorkSection, Contact, MyContactInfo
 
 # Create your views here.
 def index(request):
-    return render(request, 'portfolio/index.html')
+    home = Home.objects.first()
+    about = About.objects.first()
+    skill_section = SkillSection.objects.first()
+    skills = Skill.objects.all()
+    work_section = WorkSection.objects.first()
+    contact = Contact.objects.first()
+    my_contact_info = MyContactInfo.objects.first()
+    return render(request, 'portfolio/index.html', {'home': home, 'about': about, 'skill_section': skill_section, 'skills': skills, 'work_section': work_section, 'contact': contact, 'my_contact_info': my_contact_info})
